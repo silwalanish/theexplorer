522e08ef7d9120456d0a5b13a180976eee3b2659
silwalanish <silwalanish@gmail.com>
2024-08-24 18:44:00 +0545

    chore: A small step for man, huge step for mankind.

--- game/src/scenes/ShowcaseScene.cpp
+++ game/src/scenes/ShowcaseScene.cpp
@@ -11,7 +11,6 @@
 #include <ecs/components/Mesh.hpp>
 #include <ecs/components/Transform.hpp>
 #include <game/scripts/EditorCameraController.hpp>
-#include <game/scripts/TerrainGenerator.hpp>
 #include <scripting/ScriptableEntity.hpp>
 #include <scripting/component/NativeScript.hpp>
 
@@ -36,26 +35,8 @@ void ShowcaseScene::OnInit()
     ScriptableEntity camera(world.get());
     camera.addComponent<Camera>(Camera { 0.01f, 1000.0f, 60.0f, 1.33f, true });
     camera.addComponent<Transform>(Transform { glm::vec3(0.0f, 50.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f) });
-    camera.addScript<EditorCameraController>(30.0f, 1.0f);
+    camera.addScript<EditorCameraController>(30.0f, 5.0f);
     setActiveCamera(camera.getHandle());
-
-    HeightMap heightMap(0.01f, 15.0f, 4.0f, 0.1f);
-    float chunkSize = 10.0f;
-    int chunkPerRow = 5;
-    glm::vec3 startChunk(-(chunkPerRow - 1) * (chunkSize / 2.0f), 0.0f, -(chunkPerRow - 1) * (chunkSize / 2.0f));
-
-    Entity terrain(world.get());
-    terrain.addComponent<Transform>(Transform { startChunk, glm::vec3(0.0f), glm::vec3(1.0f) });
-
-    for (int i = 0; i < chunkPerRow; i++) {
-        for (int j = 0; j < chunkPerRow; j++) {
-            ScriptableEntity terrainChunk(world.get());
-            terrainChunk.addComponent<Transform>(Transform { glm::vec3(i * chunkSize, 0.0f, j * chunkSize), glm::vec3(0.0f), glm::vec3(1.0f) });
-            terrainChunk.addScript<TerrainGenerator>(25, chunkSize, heightMap);
-
-            terrain.addChild(terrainChunk.getHandle());
-        }
-    }
 
     Entity sun(world.get());
     sun.addComponent<DirectionalLight>(DirectionalLight { Light { 0.05f, glm::vec3(0.8f, 0.8f, 0.9f), glm::vec3(0.0f, -0.25f, 0.5f) } });
@@ -72,46 +53,6 @@ void ShowcaseScene::OnUpdate(float deltaTime)
     if (m_debugRender) {
         m_debugRenderer->render();
     }
-
-    // std::ofstream file("debug.log");
-    // std::queue<EntityHandle> entites;
-    // entites.push(m_world->getSceneGraph()->getRoot());
-
-    // while (entites.size() > 0) {
-    //     EntityHandle entity = entites.front();
-    //     entites.pop();
-
-    //     Transform& transform = m_world->getComponent<Transform>(entity);
-    //     if (m_world->getSceneGraph()->isRoot(entity)) {
-    //         file << "ID: " << entity << std::endl
-    //              << "\tPOSITION: " << glm::to_string(transform.position) << std::endl
-    //              << "\tROTATION: " << glm::to_string(transform.rotation) << std::endl
-    //              << "\tSCALE: " << glm::to_string(transform.scale) << std::endl
-    //              << "\tLOCAL MATRIX: " << glm::to_string(transform.getLocalMatrix()) << std::endl
-    //              << "\tMODEL MATRIX: " << glm::to_string(transform.getModelMatrix()) << std::endl
-    //              << "\tPARENT ID: ---ROOT---" << std::endl;
-    //     } else {
-    //         EntityHandle parent = m_world->getParent(entity);
-    //         Transform& parentTransform = m_world->getComponent<Transform>(parent);
-
-    //         file << "ID: " << entity << std::endl
-    //              << "\tPOSITION: " << glm::to_string(transform.position) << std::endl
-    //              << "\tROTATION: " << glm::to_string(transform.rotation) << std::endl
-    //              << "\tSCALE: " << glm::to_string(transform.scale) << std::endl
-    //              << "\tLOCAL MATRIX: " << glm::to_string(transform.getLocalMatrix()) << std::endl
-    //              << "\tMODEL MATRIX: " << glm::to_string(transform.getModelMatrix()) << std::endl
-    //              << "\tPARENT ID: " << parent << std::endl
-    //              << "\tPARENT MODEL MATRIX: " << glm::to_string(parentTransform.getModelMatrix()) << std::endl;
-    //     }
-
-    //         for (EntityHandle child : m_world->getChildren(entity)) {
-    //             entites.push(child);
-    //         }
-    // // }
-
-    // file.close();
-
-    // m_application->stop();
 }
 void ShowcaseScene::OnMouseDown(MouseButtonDownEvent* event)
 {
--- renderer/src/RenderableScene.cpp
+++ renderer/src/RenderableScene.cpp
@@ -37,14 +37,14 @@ std::shared_ptr<World> RenderableScene::getWorld() const
 
 void RenderableScene::setSun(EntityHandle sun)
 {
-    assert(m_world->hasComponent<DirectionalLight>(sun) && "Entity needs to have 'DirectionalLight' component.");
+    assert(m_world->hasComponent<DirectionalLight>(sun) && "Scene needs to have 'DirectionalLight' component.");
 
     m_sun = sun;
 }
 
 void RenderableScene::setActiveCamera(EntityHandle camera)
 {
-    assert(m_world->hasComponent<Camera>(camera) && "Entity needs to have 'Camera' component.");
+    assert(m_world->hasComponent<Camera>(camera) && "Scene needs to have 'Camera' component.");
 
     m_activeCamera = camera;
 }
